dd9d84033dee3477b92e8448ddcf5bdce4b8a615
Red Chou <krmgosh0404@hotmail.com.tw>
2017-12-28 16:34:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
@@ -15,6 +16,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on: ' port);
 });
